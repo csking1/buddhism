68b9ef48c806136177ec1f59a2f8b43ab1886e13
Charity Susan King <csking1@cs.uchicago.edu>
2016-05-20 12:12:51 -0500

    Query Engine Complete

--- nyt/articles.py
+++ nyt/articles.py
@@ -22,16 +22,48 @@ def parse_to_csv(json_dict):
     articles = json_dict["response"]["docs"]
     for each in articles:
         print ("--------------------------------------------------------------")    
-        summary = each["summary"]
+        summary = each["lead_paragraph"]
         url= each["web_url"]
         date = each["pub_date"]
         keywords_l = []
         keywords = each["keywords"]
         for each in keywords:
-            word = each["value"]
-            print (word)
-
+            keywords_l.append(each["value"])
+        
 def go(key, query):
+    '''
+    Wrapper function that takes api key, string query
+    Calls make_connection and received an article dictionary
+    Calls parse_to_csv to grab important information out of each inner dictionary
+    '''
+
+    csv_file = "results/" + query.strip() + ".csv"
+    with open(csv_file, 'w', newline="") as csvfile:
+        w = csv.writer(csvfile, delimiter=',')
+        w.writerow(["Date", "Summary", "Keywords", "Url" ])
+        for i in range(10):
+            try:
+                json_dict = make_connection(key, query)
+                articles = json_dict["response"]["docs"]
+                for each in articles:
+                    print ("--------------------------------------------------------------")    
+                    summary = each["lead_paragraph"]
+                    url= each["web_url"]
+                    date = each["pub_date"]
+                    keywords_l = []
+                    keywords = each["keywords"]
+                    for each in keywords:
+                        keywords_l.append(each["value"])
+                    w.writerow([date, summary, keywords_l, url])
+
+
+            except:
+                print("Connection Failed")
+
+            
+
+
+def cycle(key, query):
     '''
     Wrapper function that takes api key, string query
     Calls make_connection and received an article dictionary
