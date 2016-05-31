7da03034cd5445970b90ecd7a91edd93953f591a
Emily Webber <egwebber@cs.uchicago.edu>
2016-05-31 18:00:57 -0500

    Training set is fully scaled

--- Finding/Data/clean_and_send_to_main_test.py
+++ Finding/Data/clean_and_send_to_main_test.py
@@ -8,31 +8,41 @@ import sys
 from nltk.corpus import stopwords
 
 
-def clean_word(letter):
-	for each in ["(", ".", ',', ")"]:
+def clean_word(letter, filename):
+	letter = letter.lower()
+	if "trump" not in filename:
+		for each in ["(", ".", ',', ")", "'"]:
+			if each in letter:
+				letter = letter.replace(each, "")
+		if len(letter) <= 1:
+			return None
+		return letter
+
+	for each in ["?", "-", ":", "cheers", "applause", "(", ")", ".", ",", "'", '"', "--", ";"]:
 		if each in letter:
 			letter = letter.replace(each, "")
-	if len(letter) == 0:
-		return None
+	if len(letter) <= 1:
+			return None
 	return letter
 
 
-def process(filename):
+def process(filename, label):
 	'''
 	Reads in each line from the text file, tokenizes the sentence,
 	Looks for stop words, sends results to main training text file
 	'''
-
 	stop = stopwords.words('english')
 	stop.append(None)
 	with open(filename, "r") as f:
 		for l in f.readlines():
 			out = []
 			for c in l.split():
-				s = clean_word(c)
+				s = clean_word(c, filename)
 				if s not in stop:
 					out.append(s)
-			print (out)
+			if len(out) > 1:
+				out.append(label)
+				print (" ".join(out))
 
 if __name__ == "__main__":
-	process(sys.argv[1])
+	process(sys.argv[1], sys.argv[2])
