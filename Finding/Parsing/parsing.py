da8b173f995f4c9692805f8de296af1dca941479
Emily Webber <egwebber@cs.uchicago.edu>
2016-05-31 20:32:32 -0500

    trying parser

--- Finding/Parsing/parsing.py
+++ Finding/Parsing/parsing.py
@@ -11,11 +11,9 @@ def read_files():
 
 	os.chdir("/home/ec2-user/gutenberg_text")
 	for file in glob.glob("*.txt"):
-		with open(file, "r") as f:
-			print (f.readline())
+		print ("Found {}".format(file))
 		
 
 if __name__ == '__main__':		
-
 	read_files()
 
